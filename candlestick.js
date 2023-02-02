aa442f75884f7a6005aac89d2ac4cf6de81ddc66
jkspytest <jkspy1122@gmail.com>
2023-02-03 01:41:40 +0800

    integrated klinesavetoDb feature with duplicate key update

--- candlestick.js
+++ candlestick.js
@@ -1,24 +1,25 @@
 const getJson = require('./getJson');
 
-async function saveKlinedataToDb(parsedJsonData) {
+async function saveklineToDb(parsedJsonData) {
     if (parsedJsonData === null) {
         console.log("You need to get json data first by calling getJson()");
         return;
     }
-    parsedJsonData.map(d => {
-        const openTime1 = d[0];
-        interval = '1m';
-        open = d[1];
-        high = d[2];
-        low = d[3];
-        close = d[4];
-        volume = d[5];
-        closeTime = d[6];
-        quoteAssetVolume = d[7];
-        numbersOfTrades = d[8];
-        takerBuyBaseAssetVolume = d[9];
-        takerBuyQuoteAssetVolume = d[10];
-        let sql = "INSERT INTO BTCUSDT (`open_time`,`interval`,`open`,`high`,`low`,`close`,`volume`,`close_time`,`quote_asset_volume`,`number_of_trades`,`taker_buy_base_asset_volume`,`taker_buy_quote_asset_volume`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+    let finalData = parsedJsonData
+    finalData.map(async d => {
+        const openTime1 = d[0],
+            interval = '1m',
+            open = d[1],
+            high = d[2],
+            low = d[3],
+            close = d[4],
+            volume = d[5],
+            closeTime = d[6],
+            quoteAssetVolume = d[7],
+            numbersOfTrades = d[8],
+            takerBuyBaseAssetVolume = d[9],
+            takerBuyQuoteAssetVolume = d[10];
+        let sql = `INSERT INTO ${processingSymbol} (\`open_time\`,\`interval\`,\`open\`,\`high\`,\`low\`,\`close\`,\`volume\`,\`close_time\`,\`quote_asset_volume\`,\`number_of_trades\`,\`taker_buy_base_asset_volume\`,\`taker_buy_quote_asset_volume\`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)`;
         let values = [openTime1, interval, open, high, low, close, volume, closeTime, quoteAssetVolume, numbersOfTrades, takerBuyBaseAssetVolume, takerBuyQuoteAssetVolume];
         pool.query(sql, values, function (err, result) {
             if (err) throw err;
@@ -26,3 +27,7 @@ async function saveKlinedataToDb(parsedJsonData) {
         })
     })
 }
+
+module.exports.saveklineToDb = saveklineToDb;
+
+saveklineToDb();
--- parameterGenerator.js
+++ parameterGenerator.js
@@ -1,4 +1,5 @@
 const bottleneck = require('bottleneck');
+const { indexOf, slice } = require('lodash');
 const _ = require('lodash');
 const getJson = require('./getJson.js');
 const log = console.log;
@@ -6,6 +7,7 @@ const log = console.log;
 const sql_initObj = require('./sql_init.js');
 const pool = sql_initObj.pool;
 const promisequery = sql_initObj.promisequery;
+
 //initial limiter for klinedownloader
 const limiter = new bottleneck({
   reservoir: 5, // initial value
@@ -14,6 +16,9 @@ const limiter = new bottleneck({
   maxConcurrent: 1,
   minTime: 3000,
 });
+
+
+
 let wrapGetJson = limiter.wrap(getJson);
 const tickers = ['BTCUSDT','ETHUSDT'];
 const config = {
@@ -21,7 +26,7 @@ const config = {
   symbolArray: tickers,
   pendingSymbolArray: [],
   defaultFromTS:'09/08/2019 00:00:00',//Format - mm/dd/yyyy hh:mm:ss;
-  toTS:'10/10/2019 10:10:00',//Format - mm/dd/yyyy hh:mm:ss;
+  toTS:'2/3/2023 1:35:00',//Format - mm/dd/yyyy hh:mm:ss;
   ///////////////////////////////////////////////////////////////////////////
   tfw:{
     '1m':1*60*1000,
@@ -71,12 +76,12 @@ const getReqArray = ({ symbolArray, fromTS, toTS, timeframe, tfw }) => {
         const n = Math.ceil((toTS - fromTS) / (1500 * barw));
         if (n <=1 ) {
           const limits = Math.ceil((toTS - fromTS) / barw);
-          return `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${timeframe}&startTime=${fromTS}&limit=${limits}`
+          return [`https://fapi.binance.com/fapi/v1/klines?symbol=${symbol}&interval=${timeframe}&startTime=${fromTS}&limit=${limits}`] //if n<=1 , the default return value will be strings inserted in 1 array. Have to modify the function to return each reqParameter string in each array. 
         }
         else 
         return _.times(n, (i) => {
             const startTS = fromTS + i * 1500 * barw;
-            return `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${timeframe}&startTime=${startTS}&limit=1500`;
+            return `https://fapi.binance.com/fapi/v1/klines?symbol=${symbol}&interval=${timeframe}&startTime=${startTS}&limit=1500`;
         })
     })
 
@@ -86,16 +91,61 @@ const getReqArray = ({ symbolArray, fromTS, toTS, timeframe, tfw }) => {
 //config.getPrameters會輸出一個物件，內含symbol,timeframe,fromTS,toTS,fileName,tfw(物件)
 //可定一個物件來存放轉出的parameters
 //getReq可將gerPrameters得到的物件，當作參數傳入進行處理
-async function run() {
-  const pendingReqArray = await getReqArray(await config.getPrameters('1m'));
-  const processingSymbolArray = config.pendingSymbolArray
+async function run(yourInterval) {
+  const pendingReqArray = await getReqArray(await config.getPrameters(yourInterval));
+  //const processingSymbolArray = config.pendingSymbolArray
   log(pendingReqArray);
   await Promise.all(pendingReqArray.map(async k => {
+    //let processingSymbol = processingSymbolArray[i]
     await Promise.all(k.map(async i => {
-      console.log(await wrapGetJson(i));
+      let processingSymbol = i.slice((i.indexOf('symbol=')+7),i.indexOf('&interval'))
+      //og(processingSymbol);
+      let parsedJsonData = await wrapGetJson(i);
+      (async (parsedJsonData) => {
+        //log(parsedJsonData);
+        if (parsedJsonData === null) {
+            console.log("You need to get json data first by calling getJson()");
+            return;
+        }
+        parsedJsonData.map(async d => {
+            const openTime1 = d[0],
+                interval = yourInterval,
+                open = d[1],
+                high = d[2],
+                low = d[3],
+                close = d[4],
+                volume = d[5],
+                closeTime = d[6],
+                quoteAssetVolume = d[7],
+                numbersOfTrades = d[8],
+                takerBuyBaseAssetVolume = d[9],
+                takerBuyQuoteAssetVolume = d[10];
+            let sql = `INSERT INTO ${processingSymbol} (\`open_time\`,\`interval\`,\`open\`,\`high\`,\`low\`,\`close\`,\`volume\`,\`close_time\`,\`quote_asset_volume\`,\`number_of_trades\`,\`taker_buy_base_asset_volume\`,\`taker_buy_quote_asset_volume\`) VALUES (?,?,?,?,?,?,?,?,?,?,?,?) ON DUPLICATE KEY UPDATE \`open\` = ${open},\`high\` = ${high},\`low\` = ${low},\`close\` = ${close},\`volume\` = ${volume},\`close_time\` = ${closeTime},\`quote_asset_volume\` = ${quoteAssetVolume},\`number_of_trades\` = ${numbersOfTrades},\`taker_buy_base_asset_volume\` = ${takerBuyBaseAssetVolume},\`taker_buy_quote_asset_volume\` = ${takerBuyQuoteAssetVolume}`;
+            let values = [openTime1, interval, open, high, low, close, volume, closeTime, quoteAssetVolume, numbersOfTrades, takerBuyBaseAssetVolume, takerBuyQuoteAssetVolume];
+            pool.query(sql, values, function (err, result) {
+                if (err) {throw err;
+                console.log(err);}
+            })
+        })
+      })(parsedJsonData);
+      console.log(`${yourInterval} ${processingSymbol} fetch done`);
     }))
   }))
-  console.log('done time' + " ---- " + (new Date().toLocaleString()) + " ---- ")
+  console.log(`${yourInterval} data done time ---- ${(new Date().toLocaleString())}  ---- `)
 }
 
-run();
+
+//run('1d');
+//run('12h');
+//run('8h');
+run('2h');
+//run('15m');
+
+
+// function symbolextractor(i){
+//   log(i.indexOf('symbol='));
+//   log(i.indexOf('&interval'))
+//   log(i.slice((i.indexOf('symbol=')+7),(i.indexOf('&interval')-1)))
+// }
+
+// symbolextractor('https://api.binance.com/sadasdapi/v3/klines?symbol=BTCUSDT&interval=15m&startTime=1567872000000&limit=1500');
